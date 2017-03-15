ead58453e8bae14c91cc63ba99345f6687bb0c23
Jayatejaganesula <jayatejaganesula@gmail.com>
2017-03-15 09:26:03 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ app.use(morgan('combined'));
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
-app.get('/article-teja', function (req, res) {
+app.get('/article-one', function (req, res) {
   res.send('article teja is requested by the user');
 });
 app.get('/ui/style.css', function (req, res) {
