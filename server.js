1e0000f378019e605e68adcd5a57c0557bd6c675
Jayatejaganesula <jayatejaganesula@gmail.com>
2017-03-20 16:50:05 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var morgan = require('morgan');
 var path = require('path');
-
+var crypto= require('crypto');
 var app = express();
 app.use(morgan('combined'));
 
@@ -22,7 +22,14 @@ app.get('/article-three', function (req, res) {
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
-
+funtion hash (input,salt) {
+    var hashed=crypto.pbkdf2sync(input,salt,10000,512,'sha512');
+    return hashed.tostring('hex');
+}
+app.get('/hash/:input', function(req,res)   {
+    var hashedstring=hash(req.params.input,'this-is-some-random-string');
+    res.send(hashedstring);
+});
 app.get('/ui/madi.png', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
 });
